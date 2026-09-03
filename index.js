a1de328447654a4f2c31c5d26be74f194a970403
Nazmus Sakib (Hulu) <54477339+ioryskb@users.noreply.github.com>
2026-09-04 00:13:30 +0600

    Weight conversion added (lbs to kg)
    Weight conversion added. Users can now convert pounds to kilogram and vice versa. Stored the conversion area into another div so now the back button hides this entire div and brings the user back to the menu. Added some CSS so that the buttons in the conversion menu has some spacing between them. One problem though, when a user clicks on a conversion button, I have to hide the other conversion elements and only show the respective HTML elements. This will make the JavaScript code a lot messy. Have to figure out a more efficient way to show only the HTML elements that the user needs for the conversion.

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const tempBtn = document.getElementById("temperature")
-const resultText = document.querySelector(".calculation")
+const weightBtn = document.getElementById("weight")
+const tempConversionSection = document.querySelector(".temperature-calculation")
+const weightConversionSection = document.querySelector(".weight-calculation")
+
 const backBtn = document.getElementById("back")
+const conversionArea = document.querySelector(".conversion-area")
 const conversionMenu = document.querySelector(".convertion-menu")
 const convertTempBtn = document.getElementById("convert-temp")
 
 
 const fahrenheitValue = document.getElementById("fahrenheit-value")
 const celsiusValue = document.getElementById("celsius-value")
+const poundsValue = document.getElementById("pound-value")
+const kilogramValue = document.getElementById("kilogram-value")
+
 
 tempBtn.addEventListener("click", function(){
     conversionMenu.classList.add('hide-display')
-    resultText.classList.remove('hide-display')
+    weightConversionSection.classList.add('hide-display')
+    tempConversionSection.classList.remove('hide-display')
+    conversionArea.classList.remove('hide-display')
 })
+
+weightBtn.addEventListener("click", function(){
+    conversionMenu.classList.add('hide-display')
+    tempConversionSection.classList.add('hide-display')
+    weightConversionSection.classList.remove('hide-display')
+    conversionArea.classList.remove('hide-display')
+})
+
 backBtn.addEventListener("click", function(){
     conversionMenu.classList.remove('hide-display')
-    resultText.classList.add('hide-display')
+    conversionArea.classList.add('hide-display')
 })
+
+    /* ============== Temperature calculation ================ */
 
     fahrenheitValue.addEventListener("input", () => {
         let result = (fahrenheitValue.valueAsNumber - 32) * 5/9
@@ -25,4 +44,20 @@ backBtn.addEventListener("click", function(){
         let result = (celsiusValue.valueAsNumber * 5/9) + 32
         fahrenheitValue.value = result
     })
+
+    /* ======================================================== */
+
+     /* ============== Weight calculation ================ */
+     poundsValue.addEventListener("input", () => {
+        let result = (poundsValue.valueAsNumber / 2.205)
+        kilogramValue.value = result
+    })
+    kilogramValue.addEventListener("input", () => {
+        let result = (kilogramValue.valueAsNumber * 2.205)
+        poundsValue.value = result
+    })
+
+    /* ======================================================== */
+
+
 
